5ffac5efc7d82ef23d37beeecdba417a3668e21c
César Oliveira <cesar.oliveira@takelog.com.br>
2024-10-25 15:29:05 -0300

    Create  Project WSeb

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+
+
+const name = "cesar"
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <h1>Jornada JS {name}</h1>
+    <hr/>  
+  </>
+
+)
