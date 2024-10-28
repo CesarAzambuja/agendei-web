20a7a2a04aaa1e9716356ada40519f1c1818c23a
César Oliveira <cesar.oliveira@takelog.com.br>
2024-10-28 13:30:48 -0300

    create component Herder

--- src/pages/appointments/appointments.jsx
+++ src/pages/appointments/appointments.jsx
@@ -1,10 +1,38 @@
+import { Link } from "react-router-dom"
 import Navbar from "../../components/navibar/navbar"
+import {doctors} from '../../constants/data'
+
 
 function Appointments (){
-    return <>
+    return <div className="container-fluid  mt-page">
     <Navbar />
-    <h1>Agendamentos</h1>
-    </>
+    <div className="d-flex justify-content-between align-items-center">
+        <div className=" d-flex align-items-center">
+            <h2 className="d-inline">Agendamentos</h2>
+            <Link to='/appointments/add' className="btn btn-outline-primary ms-5">Novo Agendamento</Link>
+        </div>
+        <div className="d-flex justify-content-end align-items-center">
+            <input type="date" className="form-control" id="startDate" />
+            <span className="m-2">Até</span>
+            <input type="date" className="form-control" id="endDate" />
+            <div className="form-control ms-3 me-3">
+                <select name="doctor" id="doctor">
+                    <option value=""> Todos os médicos</option>
+                    {
+                        doctors.map((doc) => {
+                            return <option
+                                value={doc.id_doctor}>
+                                {doc.name}
+                            </option>
+                        })
+                    }
+                    
+                </select>
+            </div>
+            <button className="btn btn-primary">Filtrar</button>
+        </div>
+    </div>
+    </div>
 }
 
 export default Appointments
